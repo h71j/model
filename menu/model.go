ce4fa4a1167f306c67b1197e7057b5cbeb700260
Frank <frank@Franks-iMac.local>
2023-11-26 11:03:13 +0800

    feature: update menu display

--- menu/model.go
+++ menu/model.go
@@ -36,7 +36,7 @@ type Model struct {
 	Goods     []string      `json:"goods_list" bson:"goods_list"`
 	GoodsList []GoodsConfig `json:"goods_list_v2" bson:"goods_list_v2"`
 	// GoodsDisplay 货物展示 不存储
-	GoodsDisplay []goods.Model `json:"goods_display" bson:"-"`
+	GoodsDisplay []*goods.Model `json:"goods_display" bson:"-"`
 	// 发布到的门店
 	Stores []string `json:"stores" bson:"stores"`
 	// 更新方式
