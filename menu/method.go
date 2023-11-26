8d0e1f933d05ba8de20269371e958feeab706e09
Frank <frank@Franks-iMac.local>
2023-11-26 10:14:15 +0800

    feature: add menu & goods

--- menu/method.go
+++ menu/method.go
@@ -34,3 +34,42 @@ func (m *Model) GetGoods(goodsIds []string) ([]*goods.Model, error) {
 	}
 	return results, nil
 }
+
+// GetGoodsByMenuID 后续改为批量查询
+func (m *Model) GetGoodsByMenuID(menuID string) ([]*goods.Model, error) {
+
+	err := m.GetOne(m, menuID)
+	if err != nil {
+		return nil, err
+	}
+
+	goodsIds := make([]string, 0)
+	for _, i := range m.GoodsList {
+		goodsIds = append(goodsIds, i.GoodsId)
+	}
+
+	results := make([]*goods.Model, 0)
+	coll := m.Context.Handler.Collection(m.Context.Collection)
+	objsIds := make([]*primitive.ObjectID, 0)
+	for _, i := range m.Goods {
+		objID, _ := primitive.ObjectIDFromHex(i)
+		objsIds = append(objsIds, &objID)
+	}
+
+	filter := bson.M{"$in": objsIds}
+
+	// 获取数据列表
+	cursor, err := coll.Find(m.Context.Context, filter)
+	if err == mongo.ErrNoDocuments {
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = cursor.All(m.Context.Context, &results); err != nil {
+		return nil, err
+	}
+	return results, nil
+}
