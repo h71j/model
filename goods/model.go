646eb4fe5ac57416e1454fee25e1bfaa58c440e0
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-02-23 02:57:04 +0800

    feat: add category_id

--- goods/model.go
+++ goods/model.go
@@ -56,6 +56,7 @@ type Model struct {
 	Content       string         `json:"content"`
 	UseProperty   int            `json:"use_property" bson:"use_property"`
 	IsUseProperty bool           `json:"is_use_property" bson:"is_use_property"`
+	CategoryID    string         `json:"category_id" bson:"category_id"`
 	Property      []PropertyInfo `json:"property"`
 
 	// 统计数据 & 限制
